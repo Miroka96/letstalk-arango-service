8fe92be53612a44930ab51737eb751377882f5b4
mirko.krause <mirko.krause@student.hpi.de>
2019-08-11 11:28:39 +0200

    added authentication

--- service/main.js
+++ service/main.js
@@ -7,4 +7,27 @@ module.context.use('/locations', require('./routes/locations'), 'locations');
 module.context.use('/questions', require('./routes/questions'), 'questions');
 module.context.use('/pictures', require('./routes/pictures'), 'pictures');
 module.context.use('/answers', require('./routes/answers'), 'answers');
-module.context.use('/sessions', require('./routes/sessions'), 'sessions');
+module.context.use('/auth', require('./routes/auth'), 'authentication');
+
+// provide session handling for all routes
+const sessionsMiddleware = require('@arangodb/foxx/sessions');
+const sessions = sessionsMiddleware({
+    storage: module.context.collection('sessions'),
+    transport: 'cookie'
+});
+module.context.use(sessions);
+
+
+// provide user access for all routes
+const users = module.context.collection('users');
+module.context.use(function (req, res, next) {
+    if (req.session.uid) {
+        try {
+            req.user = users.document(req.session.uid)
+        } catch (e) {
+            req.session.uid = null;
+            req.sessionStorage.save();
+        }
+    }
+    next();
+});
