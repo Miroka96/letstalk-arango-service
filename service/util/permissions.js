14f3deedd45617e7021e312e8cd33d4236fc778c
mirko.krause <mirko.krause@student.hpi.de>
2019-08-12 00:52:09 +0200

    added permission names

--- service/util/permissions.js
+++ service/util/permissions.js
@@ -1,6 +1,42 @@
 'use strict';
 
 module.exports = {
+    view_answers: 'view_answers',
+    add_answer: 'add_answer',
+    view_answer: 'view_answer',
+    change_answer: 'change_answer',
+    delete_answer: 'delete_answer',
+
+    view_facts: 'view_facts',
+    add_fact: 'add_fact',
+    view_fact: 'view_fact',
+    change_fact: 'change_fact',
+    delete_fact: 'delete_fact',
+
+    view_locations: 'view_locations',
+    add_location: 'add_location',
+    view_location: 'view_location',
+    change_location: 'change_location',
+    delete_location: 'delete_location',
+
+    view_pictures: 'view_pictures',
+    add_picture: 'add_picture',
+    view_picture: 'view_picture',
+    change_picture: 'change_picture',
+    delete_picture: 'delete_picture',
+
+    view_questions: 'view_questions',
+    add_question: 'add_question',
+    view_question: 'view_question',
+    change_question: 'change_question',
+    delete_question: 'delete_question',
+
+    view_topics: 'view_topics',
+    add_topic: 'add_topic',
+    view_topic: 'view_topic',
+    change_topic: 'change_topic',
+    delete_topic: 'delete_topic',
+
     view_users: 'view_users',
     add_user: 'add_user',
     view_user: 'view_user',
